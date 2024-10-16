79b421b4cc8661fc0c57c2e6e83104c1a8f4aefc
BilyiPJATK <s31718@pjwstk.edu.pl>
2024-10-16 17:27:09 +0200

    task done

--- src/SimpleNumericalOperations.java
+++ src/SimpleNumericalOperations.java
@@ -4,20 +4,30 @@ import static org.junit.jupiter.api.Assertions.*;
 public
 class SimpleNumericalOperations{
     public int add(int a, int b){
-        return a - b;
+        return a + b;  // didn't pass the test, have to change a-b to a+b
     }
 
     public boolean isPositive(int value){
-        return value < 0;
+        return value > 0; // didn't pass the test, have to change value < 0 to value > 0
     }
 
     public boolean isNegative(int value){
-        return value >= 0;
+        return value <= 0; // didn't pass the test, have to change value >= 0 to value <= 0
     }
 
     @Test
         public void testAddition() {
-        assertEquals(0, 0);
+        assertEquals(3, add(2,1));
+    }
+
+    @Test
+        public void testPogNeg(){
+        assertAll(
+                () -> assertFalse(isNegative(1)),
+                () -> assertTrue(isNegative(-1)),
+                () -> assertFalse(isPositive(-1)),
+                () -> assertTrue(isPositive(1))
+        );
     }
 
 }
